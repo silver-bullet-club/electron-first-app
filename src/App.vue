7f0f37dafd6f285de7978bd8e3d81b6fd79bad29
Tatsuya Sato <satoryu.1981@gmail.com>
2020-11-04 17:57:14 +0900

    アラームのJSONをウェブ上からもってこれるようにした

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,32 @@
 <template>
   <div id="app">
     <div>
+      <input type="url" v-model="alarmDataUrl">
+      <button @click="loadAlarms">Load</button>
+    </div>
+    <div>
       {{ formattedTime }}
     </div>
-
-    <button @click="alarm">
-      Notify
-    </button>
   </div>
 </template>
 
 <script>
 import soundurl from './assets/sounds/temple_bell.mp3'
 import AlarmQueue from './lib/alarm_queue'
-import { alarms } from './data/alarms.json'
 
 export default {
   name: 'App',
   data: () => {
     return {
+      alarmDataUrl: "https://raw.githubusercontent.com/silver-bullet-club/electron-first-app/main/src/data/alarms.json",
+      alarmQueue: null,
       currentTime: Date.now(),
       currentAlarm: null,
       intervalId: null
     }
   },
-  created: function() {
-    let alarmQueue = new AlarmQueue(alarms)
-    this.currentAlarm = alarmQueue.shift()
-    const startedAt = new Date()
-    const startTime = `${startedAt.getHours()}${startedAt.getMinutes()}`
-
-    console.log(startTime)
-    while (`${this.currentAlarm.hours}${this.currentAlarm.minutes}` < startTime) {
-      this.currentAlarm = alarmQueue.shift()
-    }
+  created: async function() {
+    await this.loadAlarms()
 
     const updateTime = () => {
       this.currentTime = Date.now()
@@ -41,7 +34,7 @@ export default {
       let now = new Date()
       if (this.currentAlarm && this.currentAlarm.hours == now.getHours() && this.currentAlarm.minutes == now.getMinutes()) {
         this.alarm(this.currentAlarm.message)
-        this.currentAlarm = alarmQueue.shift()
+        this.currentAlarm = this.alarmQueue.shift()
       }
     }
     this.intervalId = setInterval(updateTime, 1000);
@@ -75,6 +68,17 @@ export default {
     playSound() {
       const audio = new Audio(soundurl)
       audio.play()
+    },
+    async loadAlarms() {
+      const response = await fetch(this.alarmDataUrl).then(res => res.json())
+      this.alarmQueue = new AlarmQueue(response.alarms)
+      this.currentAlarm = this.alarmQueue.shift()
+      const startedAt = new Date()
+      const startTime = `${startedAt.getHours()}${startedAt.getMinutes()}`
+
+      while (`${this.currentAlarm.hours}${this.currentAlarm.minutes}` < startTime) {
+        this.currentAlarm = this.alarmQueue.shift()
+      }
     }
   }
 }
